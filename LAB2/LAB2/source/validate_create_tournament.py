e012ea6bc204cc6269d620a7d59cf51923948f01
lvalukvlad <144939061+lvalukvlad@users.noreply.github.com>
2025-06-04 05:28:00 +0300

    Update validate_create_tournament.py

--- LAB2/LAB2/source/validate_create_tournament.py
+++ LAB2/LAB2/source/validate_create_tournament.py
@@ -2,29 +2,24 @@ import re
 from source.created_tournament import CreatedTournament
 
 def validate_create_tournament(created_tournament: CreatedTournament) -> str:
-    # Проверка имени турнира
     name = created_tournament.get_name()
     if not name or not re.match(r"^[\w\s\-]+$", name):
         return "Неверно записано имя турнира"
 
-    # Проверка даты
     date = created_tournament.get_date()
     if not date:
         return "Дата проведения не указана"
 
-    # Проверка типа спорта
     sport_type = created_tournament.get_sport_type()
     if not sport_type or not re.match(r"^[А-ЯЁа-яё\s]+$", sport_type):
         return "Неверно указан тип спорта"
 
-    # Проверка имени победителя
     winner_name = created_tournament.get_winner_name()
     if not winner_name or not re.match(r"^[А-ЯЁ][а-яё]+(?:[-' ]?[А-ЯЁ][а-яё]+)*$", winner_name):
         return "Неверно записано имя победителя"
 
-    # Проверка призового фонда
     prize_amount = created_tournament.get_prize_amount()
     if prize_amount is None or prize_amount <= 0:
         return "Неверно указан размер призовых"
 
-    return ""  # Если ошибок нет
+    return "" 
