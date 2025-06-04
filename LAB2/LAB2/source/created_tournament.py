c7608cd4a2c20d182b60517cfc3b3317072e8f9a
lvalukvlad <144939061+lvalukvlad@users.noreply.github.com>
2025-06-04 05:25:47 +0300

    Update created_tournament.py

--- LAB2/LAB2/source/created_tournament.py
+++ LAB2/LAB2/source/created_tournament.py
@@ -22,35 +22,35 @@ class CreatedTournament:
 
     def set_name(self, name: str):
         self.__name = name.strip() if name else None
-        print(f"Set name: {self.__name}")  # Отладка
+        print(f"Set name: {self.__name}") 
 
     def get_name(self) -> str | None:
         return self.__name
 
     def set_date(self, event_date: date):
         self.__date = event_date
-        print(f"Set date: {self.__date}")  # Отладка
+        print(f"Set date: {self.__date}") 
 
     def get_date(self) -> date | None:
         return self.__date
 
     def set_sport_type(self, sport_type: str):
         self.__sport_type = sport_type.strip() if sport_type else None
-        print(f"Set sport type: {self.__sport_type}")  # Отладка
+        print(f"Set sport type: {self.__sport_type}") 
 
     def get_sport_type(self) -> str | None:
         return self.__sport_type
 
     def set_winner_name(self, winner_name: str):
         self.__winner_name = winner_name.strip() if winner_name else None
-        print(f"Set winner name: {self.__winner_name}")  # Отладка
+        print(f"Set winner name: {self.__winner_name}") 
 
     def get_winner_name(self) -> str | None:
         return self.__winner_name
 
     def set_prize_amount(self, amount: float):
         self.__prize_amount = amount if amount is not None and amount >= 0 else None
-        print(f"Set prize amount: {self.__prize_amount}")  # Отладка
+        print(f"Set prize amount: {self.__prize_amount}") 
 
     def get_prize_amount(self) -> float | None:
         return self.__prize_amount
